c920c8c780e9727698aa64d2a32046e755127a97
Yogesh Tomar <kshanu385@gmail.com>
2025-01-30 09:57:46 +0530

    REST API

--- REST API/index.js
+++ REST API/index.js
@@ -3,6 +3,7 @@ const app = express();
 const ejs = require(`ejs`);
 const mongoose = require(`mongoose`);
 const path = require(`path`);
+const methodOverride = require(`method-override`);
 const PORT = 3000;
 
 // IIFE
@@ -14,6 +15,8 @@ const PORT = 3000;
     console.log(error.message);
   }
 })();
+
+app.use(methodOverride(`_method`))
 
 app.set(`view engine`, `ejs`);
 app.use(express.static(path.resolve(__dirname, `views`)));
@@ -39,6 +42,31 @@ app.get(`/product/new`, (req, res) => {
 app.post(`/products`, (req, res) => {
   const { name, price, stock } = req.body;
   const newProduct = Product.create({ name, price, stock });
+  res.redirect(`/products`);
+});
+
+app.get(`/products/:id`, async (req, res) => {
+  const { id } = req.params;
+  const singleProduct = await Product.findById(id);
+  const { name, price, stock } = singleProduct;
+  res.render(`singleProduct.ejs`, { id, name, price, stock });
+});
+
+app.get(`/products/:id/edit`, async (req, res) => {
+  const { id } = req.params;
+  const oldProduct = await Product.findById(id);
+  const { name, price, stock } = oldProduct;
+  res.render(`edit.ejs`, { id, name, price, stock });
+});
+
+app.put(`/products/:id`, async (req, res) => {
+  const { id } = req.params;
+  const { name, price, stock } = req.body;
+  const updatedProduct = await Product.findByIdAndUpdate(id, {
+    name,
+    price,
+    stock,
+  });
   res.redirect(`/products`);
 });
 
